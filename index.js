247610fa255767ad6ebb5cf84f7d019c1ca9c745
grinko-kristina bc2024-5 <grnkkristina@gmail.com>
2024-11-28 13:57:14 +0200

    2 part get, put, delete

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const options = program.opts();
 const app = express();
 app.get('/notes/:noteName', (req, res) => {
     const noteName = req.params.noteName;
-    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
+    const notePath = path.join(options.cache, `${noteName}.txt`);
 
     if (!fs.existsSync(notePath)) {
         return res.status(404).send('Нотатка не знайдена');
@@ -25,6 +25,24 @@ app.get('/notes/:noteName', (req, res) => {
     fs.readFile(notePath, 'utf8', (err, data) => {
         res.send(data);
     });
+});
+app.put('/notes/:noteName', (req, res) => {
+    const noteName = req.params.noteName;
+    const notePath = path.join(options.cache, `${noteName}.txt`);
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send('Нотатка не знайдена');
+    }
+    fs.writeFileSync(notePath, req.body);
+    res.status(200).send('Ноатка оновлена');
+});
+app.delete('/notes/:noteName', (req, res) => {
+    const noteName = req.params.noteName;
+    const notePath = path.join(options.cache, `${noteName}.txt`);
+    if (!fs.existsSync(notePath)){
+        return res.status(404).send('Нотатка не знайдена');
+    }
+    fs.unlinkSync(notePath);
+    res.status(200).send('Нотатка видалена');
 });
 
 app.listen(options.port, options.host, () => {
